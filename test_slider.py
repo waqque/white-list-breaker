1b1875ec6ef95d7b743f5c18560bf2496fe46dbc
waqque <tanos_hanos@mail.ru>
2026-06-27 15:48:41 +0800

    deleted test files which accidentali were added

--- test_slider.py
+++
@@ -1,6 +0,0 @@
-"""Tests for test_slider."""
-
-
-def test_placeholder():
-    """TODO: replace with real tests."""
-    assert True
--- test_test.py
+++
@@ -1,6 +0,0 @@
-"""Tests for test_test."""
-
-
-def test_placeholder():
-    """TODO: replace with real tests."""
-    assert True
